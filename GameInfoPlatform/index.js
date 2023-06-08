bb3ca6d0eea68cc12499ba80f61fc1df8d39a173
lwc <lwc084106@gmail.com>
2023-06-08 17:20:23 +0800

    tab

--- GameInfoPlatform/index.js
+++ GameInfoPlatform/index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
+import * as reactNative from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+reactNative.AppRegistry.registerComponent(appName, () => App);
