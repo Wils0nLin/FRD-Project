ca3a0475e1d1d6d7652743c38ecae0a578fdb44b
lwc <lwc084106@gmail.com>
2023-06-10 17:44:19 +0800

    style sheet update

--- GameInfoPlatform/src/componets/navigation/tab/consumer/consumerAppScreen.tsx
+++ GameInfoPlatform/src/componets/navigation/tab/consumer/consumerAppScreen.tsx
@@ -2,10 +2,10 @@ import {Button} from '@ui-kitten/components';
 import React from 'react';
 import {Text, View} from 'react-native';
 import SearchStack, {TopNavigation} from '../../topBar';
-
+import Sharedstyle from '../../../../assets/styleSheets/StyleSheet';
 const ConsumerAppScreen = (navigation: any) => {
   return (
-    <View>
+    <View style={Sharedstyle.container}>
       <Text>Consumer App Screen</Text>
       {/* <Button onPress={()=>{()=>Alerts}}>TEST</Button> */}
     </View>
